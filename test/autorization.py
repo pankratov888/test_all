e2328fd2c27cd8215da42c87c9b0685da2247db0
pankratov888 <pankratov@evolenta.ru>
2024-07-12 14:39:49 +0300

    push 40

--- test/autorization.py
+++ test/autorization.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time
 import os
-
 
 
 # Укажите путь к исполняемому файлу Chrome-Gost
