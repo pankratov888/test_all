610a56af275519ddf7fde79bb483e9d521b1e2e0
pankratov888 <pankratov@evolenta.ru>
2024-07-17 10:21:17 +0300

    lin 3

--- test/autorization.py
+++ test/autorization.py
@@ -1,42 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-import time
-import os
-import logging
+from webdriver_manager.chrome import ChromeDriverManager
 
-# Установите уровень логирования
-logging.basicConfig(level=logging.INFO)
+# Установка ChromeDriver
+service = ChromeService(executable_path=ChromeDriverManager().install())
 
-try:
-    # Укажите путь к исполняемому файлу Chrome-Gost
-    chrome_gost_path = "C:\\Users\\pankr\\AppData\\Local\\Chromium\\Application\\chrome.exe"  # Убедитесь, что путь правильный
-    # Определите путь к расширению
-    extension_path = os.path.join(os.path.dirname(__file__), '../extensions/1.2.13_0.crx')  # Замените на ваше имя расширения
-    binary_yandex_driver_file = os.path.join(os.path.dirname(__file__), '../bin/chromedriver')
+# Инициализация веб-драйвера
+driver = webdriver.Chrome(service=service)
 
-    options = Options()
-    options.binary_location = chrome_gost_path
-    options.add_experimental_option('detach', True)
-    options.add_extension(extension_path)
-    options.add_argument('--enable-logging')
-    options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
-    options.add_argument('--force-device-scale-factor=0.75')  # Установка масштаба
+# Ваш тестовый код
+driver.get("http://ya.ru")
+print(driver.title)
 
-    service = ChromeService(executable_path=binary_yandex_driver_file)
-    driver = webdriver.Chrome(service=service, options=options)
-    wait = WebDriverWait(driver, 10)
-    driver.implicitly_wait(10)
-    driver.maximize_window()
-
-    driver.get("https://ya.ru")
-    time.sleep(4)
-    driver.quit()
-
-    logging.info("Browser opened and closed successfully.")
-
-except Exception as e:
-    logging.error("An error occurred: %s", e)
+# Закрытие драйвера
+driver.quit()
