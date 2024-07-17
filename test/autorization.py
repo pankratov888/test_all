3195236c1db430fca139cad15d5e5a663c72c667
pankratov888 <pankratov@evolenta.ru>
2024-07-17 10:37:37 +0300

    lin 6

--- test/autorization.py
+++ test/autorization.py
@@ -5,7 +5,6 @@ from selenium.webdriver.chrome.options import Options
 
 # Настройка опций Chrome
 chrome_options = Options()
-chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--remote-debugging-port=9222')
