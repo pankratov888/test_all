397fef1461d578d8dcb14ea8985333206f3252db
pankratov888 <pankratov@evolenta.ru>
2024-07-17 12:36:50 +0300

    push

--- test/autorization.py
+++ test/autorization.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 # Настройки Chrome
 options = Options()
@@ -12,10 +15,19 @@ options.add_argument("--headless")
 service = ChromeService(ChromeDriverManager().install())
 browser = webdriver.Chrome(service=service, options=options)
 
-print("Открытие страницы...")
-browser.get("http://ya.ru")
-print("Страница загружена.")
+try:
+    print("Открытие страницы...")
+    browser.get("http://ya.ru")
+    print("Страница загружена.")
 
+    # Проверка наличия элемента
+    element_xpath = "/html/body/main/div[3]/form/div[3]/div/div[1]"
+    WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, element_xpath)))
+    print("Элемент отображается на странице.")
 
-# Закрытие драйвера
-browser.quit()
+except Exception as e:
+    print(f"Ошибка: {e}")
+
+finally:
+    # Закрытие драйвера
+    browser.quit()
