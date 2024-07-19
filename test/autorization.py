f901c4f8ee1b95d18cc70a12fd07b4c367d07f3c
pankratov888 <pankratov@evolenta.ru>
2024-07-19 11:18:38 +0300

    push

--- test/autorization.py
+++ test/autorization.py
@@ -18,15 +18,15 @@ browser = webdriver.Chrome(service=service, options=options)
 
 try:
     print("Открытие страницы...")
-    browser.get("http://ya.ru")
+    browser.get("https://auth.pgs.gosuslugi.ru/auth/realms/DigitalgovTorkndProd1Auth/protocol/openid-connect/auth?client_id=DigitalgovTorkndProd1Auth-Proxy&state=b6fa62fc48c9м04787fa5bf095da2bafa&nonce=8bf3d529b0af28816d18e97bf560c4d3&response_type=code&redirect_uri=https%3A%2F%2Fpgs.gosuslugi.ru%2Fopenid-connect-auth%2Fredirect_uri&scope=openid")
     print("Страница загружена.")
 
     # Вывод HTML страницы для отладки
     print(browser.page_source)
 
-    # Проверка наличия элемента по атрибуту data-hydration-id
-    hydration_id = "462861dcca5139608a00d9aef9841f77.0"
-    WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, f"[data-hydration-id='{hydration_id}']")))
+    # Проверка наличия элемента по id
+    element_id = "zocial-esia"
+    WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.ID, element_id)))
     print("Элемент отображается на странице.")
 
 except Exception as e:
