2e10160c5c105eaa2d738c07a48669abc6c2b8ea
pankratov888 <pankratov@evolenta.ru>
2024-07-19 10:15:36 +0300

    push

--- test/autorization.py
+++ test/autorization.py
@@ -10,22 +10,10 @@ from selenium.webdriver.common.by import By
 
 # Настройки Chrome
 options = Options()
-# Пример: открыть браузер в режиме без головы (без графического интерфейса)
-#options.add_argument("--headless")
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')
-options.add_argument('--window-size=1024x768')
-options.add_argument('--ssl-version-min=tls1.2')
+options.add_argument("--headless")
 
-
-# Путь к расширению
-extension_path = './extensions/1.2.13_0.crx'
-options.add_extension(extension_path)
-
-
-# Инициализация веб-драйвера с использованием ChromeDriverManager
-service = ChromeService('./bin/chromedriver')
+# Инициализация веб-драйвера
+service = ChromeService(ChromeDriverManager().install())
 browser = webdriver.Chrome(service=service, options=options)
 
 try:
@@ -37,7 +25,7 @@ try:
     print(browser.page_source)
 
     # Проверка наличия элемента по атрибуту data-hydration-id
-    hydration_id = "576211ff6da7ce6ac7272570289f34fc.0"
+    hydration_id = "462861dcca5139608a00d9aef9841f77.0"
     WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, f"[data-hydration-id='{hydration_id}']")))
     print("Элемент отображается на странице.")
 
