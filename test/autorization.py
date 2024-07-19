db3a1a2341d3b91015c8c135d960931b01ac66aa
pankratov888 <pankratov@evolenta.ru>
2024-07-19 09:44:22 +0300

    push 2

--- test/autorization.py
+++ test/autorization.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
@@ -28,14 +30,14 @@ browser = webdriver.Chrome(service=service, options=options)
 
 try:
     print("Открытие страницы...")
-    browser.get("https://auth.pgs.gosuslugi.ru/auth/realms/DigitalgovTorkndProd1Auth/protocol/openid-connect/auth?client_id=DigitalgovTorkndProd1Auth-Proxy&state=b6fa62fc48c9м04787fa5bf095da2bafa&nonce=8bf3d529b0af28816d18e97bf560c4d3&response_type=code&redirect_uri=https%3A%2F%2Fpgs.gosuslugi.ru%2Fopenid-connect-auth%2Fredirect_uri&scope=openid")
+    browser.get("https://esia.gosuslugi.ru/login/")
     print("Страница загружена.")
 
     # Вывод HTML страницы для отладки
     print(browser.page_source)
-
+    time.sleep(5)
     # Проверка наличия элемента по id
-    element_id = "zocial-esia"
+    element_id = "logo"
     WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.ID, element_id)))
     print("Элемент отображается на странице.")
 
